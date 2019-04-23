63d892d4ae804b94a523caed6ba9657a9ebba92d
Mikhail Kopylov <mih.kopylov@yandex.ru>
2019-04-23 09:02:50 +0300

    Update CORS working for `dev` profile only

--- src/main/java/ru/omickron/myspb/config/WebConfiguration.java
+++ src/main/java/ru/omickron/myspb/config/WebConfiguration.java
@@ -2,17 +2,19 @@ package ru.omickron.myspb.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("dev")
 public class WebConfiguration {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings( CorsRegistry registry ) {
-                registry.addMapping( "/**" );
+                registry.addMapping( "/**" ).allowedOrigins( "http://localhost:4200" );
             }
         };
     }
