d9d8255c5415c7807ab7b43806fe010f87e35c55
Mikhail Kopylov <mih.kopylov@yandex.ru>
2019-05-03 10:09:58 +0300

    Configure frontend BASE_URL with environment variables

--- src/main/web/src/app/services/api.ts
+++ src/main/web/src/app/services/api.ts
@@ -1,10 +1,11 @@
 import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 export class Api {
-    private static BASE_URL: string = "/rest";
+    private static BASE_URL: string = environment.baseUrl + "/rest";
     public static LOGIN: string = Api.BASE_URL + "/auth/login";
     public static PROFILE: string = Api.BASE_URL + "/profile";
     public static REASON_GROUPS: string = Api.BASE_URL + "/reasonGroups";
